2964e58846edfe294a4bc16c81c477d00338b817
michaelc <michaelc@carimus.com>
2022-03-13 21:36:54 -0400

    added handlers to enable submit and history button clicks

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,19 +5,22 @@ var displayCurrentBox = document.querySelector("#current-weather");
 var displayForecastBox = document.querySelector("#week-forecast");
 var searchHistoryBox = document.querySelector("#history-block");
 var apiCity;
+var cityLocal;
 var searchHistory = [];
 var coord = [];
 
-
-function citySearch(event) {
-    event.preventDefault();
+//function to call api with city name and return coordinates
+function citySearch(cityLocal) {
+    //clear coordinate array
     coord=[];
-    var searchTerm = cityName.value;
-    if(!searchTerm) {
+    //handle empty fields
+    if(!cityLocal) {
         alert("Please enter a city.");
     } else {
-    var citySearchUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + searchTerm + "&appid=" + apiKey + "&units=imperial";
+    //set url and call api
+    var citySearchUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + cityLocal + "&appid=" + apiKey + "&units=imperial";
     fetch(citySearchUrl).then(function(response) {
+        //if a good response, record coordinates
         if (response.ok) {
             response.json().then(function(data) {
 
@@ -27,18 +30,23 @@ function citySearch(event) {
 
                 var lon = data.coord.lon;
                 coord.push(lon);
-
+                
+                //record the name of the city returned by api for display, mitigates typos
                 apiCity = data.name;
                 coordSearch(coord);
+                //clear input field
                 cityName.value="";
             })
         } else {
+            //handle bad response, clear input
             alert("City not found. Try again.");
+            cityName.value="";
         };
         });
     };
 };
 
+//call the api again, passing coordinates to get weather data
 function coordSearch(coord) {
     var coordSearchUrl = "https://api.openweathermap.org/data/2.5/onecall?lat=" + coord[0] + "&lon=" + coord[1] + "&appid=" + apiKey + "&exclude=minutely,hourly&units=imperial";
     fetch(coordSearchUrl).then(function(response) {
@@ -46,9 +54,6 @@ function coordSearch(coord) {
             response.json().then(function(data) {
                 displayCurrent(data);
                 saveSearch();
-//console!!!!                
-                console.log(data);
-                console.log(coord);
             })
         } else {
             alert("City not found. Try again.");
@@ -56,6 +61,7 @@ function coordSearch(coord) {
     });
 };
 
+//display current weather
 function displayCurrent(data) {
     displayCurrentBox.innerHTML="";
     displayForecastBox.innerHTML="";
@@ -73,6 +79,7 @@ function displayCurrent(data) {
     humidityEl.textContent = "Humidity: " + data.current.humidity + "%";
 
     var uviEl = document.createElement("p");
+    //conditional classes created to display UV Index as a color
     if (data.current.uvi < 3) {
         uviEl.innerHTML = "UV Index: <span class='uvi-favorable'>" + data.current.uvi + "</span>";
     } else if (data.current.uvi >= 3 && data.current.uvi < 5){
@@ -90,6 +97,7 @@ function displayCurrent(data) {
     displayForecast(data);
 };
 
+//display 5 day forcast
 function displayForecast(data) {
 //from tomorrow (i=1) to five days hence (i=5), append forecast box with data tiles
 for (let i = 1; i <= 5; i++) {
@@ -104,21 +112,63 @@ for (let i = 1; i <= 5; i++) {
     };
 };
 
+//save search
 function saveSearch() {
+    //verified search term is not already in history, then create a new button
     if(!searchHistory.includes(apiCity)) {
         searchHistory.push(apiCity);
         localStorage.setItem("searches", JSON.stringify(searchHistory));
-        console.log(searchHistory);
-        console.log(apiCity);
+
         var prevSearchBtn = document.createElement("button");
         prevSearchBtn.className = "history-btn";
         prevSearchBtn.innerText = apiCity;
-        prevSearchBtn.addEventListener("click", function(event) {citySearch(event.target.textContent)});
+        //attributes added to initiate search and pass city name on click
+        prevSearchBtn.setAttribute("onclick", "historyClick()");
+        prevSearchBtn.setAttribute("search-for", apiCity);
 
         searchHistoryBox.appendChild(prevSearchBtn);
     };
 };
 
-// function loadHistory
+//retrieve cites from local storage
+function loadHistory() {
+    //retrieve data from local storage
+    var pastSearches = localStorage.getItem("searches");
+    //if data is extant, populate the variable with an array
+    if(pastSearches) {
+        searchHistory = JSON.parse(pastSearches);
 
-submitBtn.addEventListener("click", citySearch);
+        //iterate through the array of past searches and generate a button for each
+        for (var i=0; i < searchHistory.length; i++) {
+            var prevSearchBtn = document.createElement("button");
+            prevSearchBtn.className = "history-btn";
+            prevSearchBtn.innerText = searchHistory[i];
+            prevSearchBtn.setAttribute("onclick", "historyClick()");
+            prevSearchBtn.setAttribute("search-for", searchHistory[i]);
+
+        searchHistoryBox.appendChild(prevSearchBtn);
+        };
+    } else {
+        //if there is no data in local storage, do nothing
+        return false;
+    };
+};
+
+//function to pull city name from button attribute and call citySearch
+function historyClick(searchCity) {
+    cityLocal = event.target.getAttribute("search-for")
+    citySearch(cityLocal);
+};
+
+//function to handle click event and pass city name to city Search
+function submitClick(event) {
+    event.preventDefault();
+
+    cityLocal = cityName.value;
+    citySearch(cityLocal);
+};
+
+//load history on page load
+loadHistory();
+
+submitBtn.addEventListener("click", submitClick);
